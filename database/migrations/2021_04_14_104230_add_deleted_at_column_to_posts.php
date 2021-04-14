b6c2ee214183fbb82e5f480c33973439cb01f299
ffayad <ffayad@ptcdb.edu.ps>
2021-04-14 13:54:59 +0300

    modify migration and add deleted_at after body column

--- database/migrations/2021_04_14_104230_add_deleted_at_column_to_posts.php
+++ database/migrations/2021_04_14_104230_add_deleted_at_column_to_posts.php
@@ -14,7 +14,7 @@ class AddDeletedAtColumnToPosts extends Migration
     public function up()
     {
         Schema::table('posts', function (Blueprint $table) {
-            $table->timestamp('deleted_at')->nullable();
+            $table->timestamp('deleted_at')->after('body')->nullable();
         });
     }
 
